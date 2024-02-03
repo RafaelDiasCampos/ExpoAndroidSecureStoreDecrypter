624cac032aa5bd5ecc30ab210e8ccb88face042e
RafaelDiasCampos <66502547+RafaelDiasCampos@users.noreply.github.com>
2024-02-03 20:51:02 -0300

    Updated script to work with newer versions of Expo

--- decrypter.js
+++ decrypter.js
@@ -2,60 +2,50 @@
 
 Java.perform(() => {
     // Common Java Classes
-    const JSONObject = Java.use('org.json.JSONObject');
-    
-    // Expo Crypto Classes
-    const AESEncrypter = Java.use('expo.modules.securestore.SecureStoreModule$AESEncrypter');
-    const HybridAESEncrypter = Java.use('expo.modules.securestore.SecureStoreModule$HybridAESEncrypter');
-    
-    // Java KeyStore Classes
-    const SecretKeyEntry = Java.use('java.security.KeyStore$SecretKeyEntry')
-    const PrivateKeyEntry = Java.use('java.security.KeyStore$PrivateKeyEntry')
-
-    // Expo SecureStore Moduke
+    const Base64 = Java.use('android.util.Base64');
+    const GCMParameterSpec = Java.use('javax.crypto.spec.GCMParameterSpec');
+    const Cipher = Java.use('javax.crypto.Cipher');
+    const String = Java.use('java.lang.String');
+      
+    // Expo Classes
     const SecureStoreModule = Java.use('expo.modules.securestore.SecureStoreModule');
+    const AESEncrypter = Java.use('expo.modules.securestore.SecureStoreModule$AESEncrypter');
 
-    // Get function we want to hook
-    const getItemImpl = SecureStoreModule.getItemImpl;
-
-    // Hook the function
-    getItemImpl.implementation = function (key, options, promise) {
-      const prefs = this.getSharedPreferences();
+    // Hook the function 'getItemImpl' to get the encrypted value
+    SecureStoreModule.getItemImpl.implementation = function (key, options, promise) {
+      let prefs = this.getSharedPreferences();
 
       if (!prefs.contains(key)) {        
-        console.log("[-] getItemImpl called. Key: " + key + " No value found.");
+        console.log("[+] getItemImpl called. Key: " + key + " No value found.");
         return this.getItemImpl(key, options, promise);
       }
 
       // Get encrypted value from the JSON
-      const encryptedItemString = prefs.getString(key, null);
-      console.log("[-] getItemImpl called. Key: " + key + " Value: " + encryptedItemString);
-
-      // Parse the JSON and extract the scheme
-      const encryptedItem = JSONObject.$new(encryptedItemString);
-      const scheme = encryptedItem.optString('scheme');
-
-      if (scheme == null) {
-        console.log("[!] Error: Scheme is null.");
-        return this.getItemImpl(key, options, promise);
-      }
-
-      // Decrypt the value based on the scheme
-      switch (scheme) {
-        case AESEncrypter.NAME.value: 
-            const secretKeyEntry = this.getKeyEntry(SecretKeyEntry.class, this.mAESEncrypter.value, options);
-            var value = this.mAESEncrypter.value.decryptItem(encryptedItem, secretKeyEntry);
-            console.log("[+] Decrypted value with AESEncrypter. Key: " + key + " Value: " + value);
-            break;
-        case HybridAESEncrypter.NAME.value:
-            const privateKeyEntry = this.getKeyEntry(PrivateKeyEntry.class, this.mHybridAESEncrypter.value, options);
-            var value = this.mHybridAESEncrypter.decryptItem(encryptedItem, privateKeyEntry);
-            console.log("[+] Decrypted value with HybridAESEncrypter. Key: " + key + " Value: " + hybridValue);
-            break;
-        default:
-            console.log("[!] Error: Unknown scheme. Scheme: " + scheme);        
-      }
+      let encryptedItemString = prefs.getString(key, null);
+      console.log("[+] getItemImpl called. Key: " + key + " Value: " + encryptedItemString);
 
       return this.getItemImpl(key, options, promise);
+    };
+
+    // Hook AESEncrypter.decryptItem to decrypt the value
+    AESEncrypter.decryptItem.overload('expo.modules.core.Promise', 'org.json.JSONObject', 'java.security.KeyStore$SecretKeyEntry', 'expo.modules.core.arguments.ReadableArguments', 'expo.modules.securestore.AuthenticationCallback').implementation = function (promise, encryptedJson, secretKeyEntry, options, callback) {
+      let encodedContent = encryptedJson.getString("ct");
+      let iv = encryptedJson.getString("iv");
+      let tlen = encryptedJson.getInt("tlen");
+
+      let decodedContent = Base64.decode(encodedContent, 0);
+
+      let parameterSpec = GCMParameterSpec.$new(tlen, Base64.decode(iv, 0));
+      let cipher = Cipher.getInstance("AES/GCM/NoPadding");
+
+      cipher.init(2, secretKeyEntry.getSecretKey(), parameterSpec);
+
+      let decrypted = cipher.doFinal(decodedContent);
+
+      let decryptedString = String.$new(decrypted, "UTF-8");
+
+      console.log("[+] AESEncrypter.decryptItem called. Decrypted: " + decryptedString);
+
+      return this.decryptItem(promise, encryptedJson, secretKeyEntry, options, callback);
     };
   });
